26556e11bf96abfd5fef199a8834dfaf80840534
Aleksei Barsukov <barsecky@gmail.com>
2020-10-22 01:26:15 +0300

    feat: add hamburger menu

--- pages/js/script.js
+++ pages/js/script.js
@@ -1,7 +1,14 @@
-function openNav() {
-  document.getElementById("myNav").style.width = "37.5rem";
-}
+// function openNav() {
+//   document.getElementById("myNav").style.width = "37.5rem";
+// }
 
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
+// function closeNav() {
+//   document.getElementById("myNav").style.width = "0%";
+// }
+
+const burgerMenu = document.querySelector('.burger__menu'),
+burgerBtn = document.querySelector('.burger__btn');
+
+burgerBtn.addEventListener('click', () => { 
+  burgerMenu.classList.toggle('openMenu')
+});
